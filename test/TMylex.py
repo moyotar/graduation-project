eb7c8e4a70a42b5602b12de44a341276e2bfc741
moyotar <792136980@qq.com>
2017-05-12 10:03:27 +0800

    增加词法测试样例

--- test/TMylex.py
+++ test/TMylex.py
@@ -14,12 +14,14 @@ ksjdf a __sd b_ a_2_1__ ACKD
 
 # test String
 "ssdfdssmem" "\\1\\2\\n\\e" "s'd\\"k\rs"
-"\1\2\3" "" "\t
-"
+"\1\2\3" "" "\t" "'\\a\\b\\f\\r\\n\\v\\t"
+"\\x0a\\x31\\xef\\xff"
+
 # test Reserved
 for in if else while
  func break continue return and
  or not nil true false local
+winapi to_str
 
 # test Eq
 ==
